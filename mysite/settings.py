c40fb8b730fa5cae8b835f5447a4dbe08fa75dff
SATLUCIFERAN <phanwiti@hotmail.com>
2025-07-18 01:42:50 +0700

    Remove WhiteNoise, preparing for Render Static Site service

--- mysite/settings.py
+++ mysite/settings.py
@@ -70,8 +70,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -158,8 +157,6 @@ STATIC_URL = '/static/'
 # NEW: Configuration for serving static files in production
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# WhiteNoise configuration for serving static files in production
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # <--- ADD THIS LINE
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -16,24 +16,3 @@
 # application = get_wsgi_application()
 
 
-####################### deploy ###########################
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-# Import settings to access STATIC_ROOT
-from django.conf import settings # <--- ADD THIS LINE
-
-# Import WhiteNoise
-from whitenoise import WhiteNoise # <--- CHANGE THIS IMPORT (from whitenoise.middleware to whitenoise)
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-application = get_wsgi_application()
-
-# NEW: Configure WhiteNoise to serve static files from STATIC_ROOT
-application = WhiteNoise(application, root=settings.STATIC_ROOT) # <--- CHANGE THIS LINE
-# Optional: If you want to enable compression and caching (recommended for production)
-# application.add_files(settings.STATIC_ROOT, prefix='static/') # This is often handled by CompressedManifestStaticFilesStorage
